e8f0a998c8147716daa09af69d715dc54db34d41
Adedamola Omoniyi <131362017+Damola-Omoniyi@users.noreply.github.com>
2025-03-12 20:24:36 +0000

    Update server-pico.py

--- server-pico.py
+++ server-pico.py
@@ -1,9 +1,7 @@
-from ws import WS_Server
+from ws import WS_Server # The ws module is a special specific module for the sunfounder car not too sure how it works
 import json
 import time
-import pico_4wd as car
-
-
+import pico_4wd as car # FOR CAR FUNCTIONS
 # Custom WebSocket Server Class
 class CustomWSServer(WS_Server):
     def on_receive(self, data):
@@ -14,15 +12,10 @@ class CustomWSServer(WS_Server):
                 car.move(cmd[0], cmd[1])
             elif cmd[0] in ["stop", "STOP"]:
                 car.move("stop")
-            
-            
-
 # Initialize WebSocket Server
-ws = CustomWSServer(name="PicoW", mode="sta", ssid="Damola's A05s", password="Overdose")
-
+ws = CustomWSServer(name="PicoW", mode="sta", ssid="", password="O")
 if ws.start():
     print("WebSocket server running...")
-
 # Run WebSocket Loop
 while True:
     ws.loop()
