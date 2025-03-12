b9d132dfdb3f0c05a33edc1902c9fc5207eef08b
Adedamola Omoniyi <131362017+Damola-Omoniyi@users.noreply.github.com>
2025-03-12 20:24:31 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -5,7 +5,7 @@ import websockets
 import json
 
 async def send_command(command):
-    uri = "ws://192.168.234.21:8765"  # Replace with Pico’s IP
+    uri = "ws:"  # Replace with Pico’s IP
     async with websockets.connect(uri) as websocket:
         await websocket.send(json.dumps({"command": command}))
         print(f"Sent: {command}")
